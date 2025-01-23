707f0ee852743a88cc423830b7f30d766d6b87ad
mbali-18 <faithfulservent18@gmail.com>
2025-01-23 22:09:12 +0200

    first commit

---
+++ sandbox.js
@@ -0,0 +1,20 @@
+// Exploring variables
+let myName = "Mbali";
+let age = 22;
+let favoriteColor = "Purple";
+let isTall = true;
+
+// Fun facts about me
+let numberOfSiblings = 3;
+let based = " Cape Town";
+let favoriteFood = "Pizza";
+let loveofmylife = "Mbuso";
+
+console.log("Hi, my name is " + myName);
+console.log("I am " + age + " years old");
+console.log("My favorite color is " + favoriteColor);
+console.log("Am I tall? " + isTall);
+console.log("I have " + numberOfSiblings + " siblings");
+console.log("I live in" + based);
+console.log("My favorite food is " + favoriteFood);
+console.log("Love of my life Mbuso")
